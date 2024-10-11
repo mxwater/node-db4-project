55e724bc1ae955ae2b0d968f71a9cfc0b1d3e9af
“Mariana <marysiq@hotmail.com>
2024-10-11 15:20:11 -0700

    wip

---
+++ data/seeds/01-cleanup.js
@@ -0,0 +1,22 @@
+const { clean } = require ('knex-cleaner')
+
+exports.seed = function (knex) {
+    return clean(knex, {
+        mode: "truncate",
+        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+    })
+}
+
+exports.seed = function (knex) {
+    return clean(knex, {
+        mode:  'truncate',
+        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+    })
+}
+
+exports.seed = function (knex) {
+    return clean(knex, {
+        mode:  'truncate',
+        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+    })
+}
