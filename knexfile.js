3a827206409dd0fd81d5baa7aa0f2491f4698197
“Mariana <marysiq@hotmail.com>
2024-10-10 18:05:37 -0700

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,19 @@
+const sharedConfig = {
+  client: 'sqlite3',
+  migrations: { directory: './data/migrations' },
+  useNullAsDefault: true,
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+};
+
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './data/recipes.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './migrations',
-      },
-      seeds: {
-        directory: './seeds',
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done);
-        },
-      },
-    },
-  };
-  
+  development: {
+    ...sharedConfig,
+    connection: { filename: './data/cook_book.db3' },
+    seeds: { directory: './data/seeds' },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/cook_book.test.db3' },
+  },
+  production: {},
+};
