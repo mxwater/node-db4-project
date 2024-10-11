3c998d76887124101f5c0bdcc8b34b9f7f61d2ce
“Mariana <marysiq@hotmail.com>
2024-10-11 15:06:38 -0700

    wip

--- data/migrations/20241011205822_initial-migration.js
+++ data/migrations/20241011205822_initial-migration.js
@@ -6,37 +6,37 @@ exports.up = async function(knex) {
     })
     .createTable('ingredients', table => {
         table.increments('ingredient_id');
-        table.string('ingredient_name', 200).notNullable();
-        table.string('quantity', 100).notNullable();
+        table.string('ingredient_name', 200).notNullable().unique();
+        table.string('ingredient_unit', 50);
     })
     .createTable('steps', table => {
         table.increments('step_id');
+        table.string('step_text', 200).notNullable();
         table.integer('step_number').notNullable();
-        table.string('instructions', 500).notNullable();
         table.integer('recipe_id')
-             .unsigned()
-             .notNullable()
-             .references('recipe_id')
-             .inTable('recipes')
-             .onDelete('CASCADE')
-             .onUpdate('CASCADE');
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT');
     })
     .createTable('step_ingredients', table => {
         table.increments('step_ingredient_id');
         table.integer('step_id')
-             .unsigned()
-             .notNullable()
-             .references('step_id')
-             .inTable('steps')
-             .onDelete('CASCADE')
-             .onUpdate('CASCADE');
+            .unsigned()
+            .notNullable()
+            .references('step_id')
+            .inTable('steps')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT');
         table.integer('ingredient_id')
-             .unsigned()
-             .notNullable()
-             .references('ingredient_id')
-             .inTable('ingredients')
-             .onDelete('CASCADE')
-             .onUpdate('CASCADE');
+            .unsigned()
+            .notNullable()
+            .references('ingredient_id')
+            .inTable('ingredients')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT');
         table.string('amount', 100);
     });
 };
