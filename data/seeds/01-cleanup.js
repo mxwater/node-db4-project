cc10cf628c3e97f416697ed93ea5e4623df8b573
“Mariana <marysiq@hotmail.com>
2024-10-11 15:25:09 -0700

    wip

--- data/seeds/01-cleanup.js
+++ data/seeds/01-cleanup.js
@@ -14,9 +14,3 @@ exports.seed = function (knex) {
     })
 }
 
-exports.seed = function (knex) {
-    return clean(knex, {
-        mode:  'truncate',
-        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
-    })
-}
