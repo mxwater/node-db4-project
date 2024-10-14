394cfe23d969c1f0df2b843ab59340c6fc6a9fec
“Mariana <marysiq@hotmail.com>
2024-10-14 09:28:18 -0700

    done

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -10,7 +10,7 @@ async function getRecipeById(recipe_id) {
       'r.recipe_name',
       's.step_id',
       's.step_number',
-      's.step_instructions as step_text', // Updated to match your migration file
+      's.step_instructions as step_text', 
       'i.ingredient_id',
       'i.ingredient_name',
       'si.quantity'
@@ -18,9 +18,39 @@ async function getRecipeById(recipe_id) {
     .orderBy('s.step_number')
     .where('r.recipe_id', recipe_id);
 
-  return recipeRows;
+  const recipe = {
+    recipe_id: recipeRows[0].recipe_id,
+    recipe_name: recipeRows[0].recipe_name,
+    steps: [],
+  };
+
+  recipe.steps = recipeRows.reduce((acc, row) => {
+    let step = acc.find(s => s.step_id === row.step_id);
+
+    if (!step) {
+      step = {
+        step_id: row.step_id,
+        step_number: row.step_number,
+        step_instructions: row.step_text,
+        ingredients: [],
+      };
+      acc.push(step); 
+    }
+
+    if (row.ingredient_id) {
+      step.ingredients.push({
+        ingredient_id: row.ingredient_id,
+        ingredient_name: row.ingredient_name,
+        quantity: row.quantity,
+      });
+    }
+
+    return acc; 
+  }, []);
+
+  return recipe;
 }
 
 module.exports = {
-  getRecipeById
+  getRecipeById,
 };
